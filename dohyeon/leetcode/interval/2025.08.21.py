60344f9ca7e5b1a43ffae056d709e7538847f2e0
Dohyeon Won <dh1072005@gmail.com>
2025-08-21 20:22:12 +0900

    더 좋은 방안 추가

--- dohyeon/leetcode/interval/2025.08.21.py
+++ dohyeon/leetcode/interval/2025.08.21.py
@@ -28,3 +28,32 @@ class Solution:
             result.append(newInterval)
             
         return result
+
+    from typing import List
+
+class BetterSolution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        result = []
+        i = 0
+        n = len(intervals)
+
+        # 1. newInterval보다 완전히 앞에 있는 간격들 추가
+        while i < n and intervals[i][1] < newInterval[0]:
+            result.append(intervals[i])
+            i += 1
+        
+        # 2. 겹치는 간격들을 병합
+        while i < n and intervals[i][0] <= newInterval[1]:
+            newInterval[0] = min(newInterval[0], intervals[i][0])
+            newInterval[1] = max(newInterval[1], intervals[i][1])
+            i += 1
+        
+        # 병합이 끝난 newInterval을 결과에 추가
+        result.append(newInterval)
+        
+        # 3. newInterval보다 완전히 뒤에 있는 간격들 추가
+        while i < n:
+            result.append(intervals[i])
+            i += 1
+            
+        return result

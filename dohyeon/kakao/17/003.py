7e36efe864c0eaec1582acddee3aac1d5702a0e9
Dohyeon Won <dh1072005@gmail.com>
2025-10-10 17:56:06 +0900

    폴더 이름 변경

--- dohyeon/kakao/17/003.py
+++ dohyeon/kakao/17/003.py
@@ -4,15 +4,29 @@ def solution(cacheSize, cities):
     
     if cacheSize == 0:
         return len(cities) * 5
-
+    
     for city in cities:
         city = city.lower()
+        
+        # cache에 city가 있는 경우
+            # 선택된 city cache에 제거 후에 다시 추가(갱신)
+            # answer += 1
+
         for i in range(len(cache)):
             if cache[i] == city:
                 cache.pop(i)
                 cache.append(city)
                 answer += 1
                 break
+        
+        # cache가 city에 없는 없는 경우
+            # cache가 가득 찬 경우
+                # cache에서 가장 오래된 city 제거 --> pop(0)
+                # 지금 city cache에 추가
+                # answer += 5
+            # cache가 가득 차지 않은 경우
+                # 지금 city cache에 추가
+                # answer += 5
         if city not in cache:
             if len(cache) == cacheSize:
                 answer += 5
@@ -23,3 +37,6 @@ def solution(cacheSize, cities):
                 cache.append(city)
 
     return answer
+
+
+

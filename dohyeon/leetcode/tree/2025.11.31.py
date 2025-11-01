d258e217658e057239de594dec6ac723dc59b81b
Dohyeon Won <dh1072005@gmail.com>
2025-11-01 11:59:56 +0900

    tree 문제

---
+++ dohyeon/leetcode/tree/2025.11.31.py
@@ -0,0 +1,23 @@
+from typing import Optional
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        if not root:
+            return True
+        return self._is_mirror(root.left, root.right)
+
+    def _is_mirror(self, left: Optional[TreeNode], right: Optional[TreeNode]) -> bool:
+        if not left and not right:
+            return True
+        if not left or not right:
+            return False
+        if left.val != right.val:
+            return False
+        return (self._is_mirror(left.left, right.right) and
+                self._is_mirror(left.right, right.left))
